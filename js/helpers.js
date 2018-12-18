107466ad029e23c80acec0d9677ea1f33acd53f3
yoshiking <yoshiking0711@gmail.com>
2018-12-18 16:10:56 +0900

    update

--- js/helpers.js
+++ js/helpers.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Helper functions
 
 // Display
@@ -50,57 +49,4 @@ InputHandeler.prototype.isPressed = function(code) {
         return this.pressed[code] = true;
     }
     return false;
-=======
-// Helper functions
-
-// Screen
-function Screen(width, height) {
-    this.canvas = document.createElement("canvas");
-    this.canvas.width = this.width = width;
-    this.canvas.height = this.height = height;
-    this.ctx = this.canvas.getContext("2d");
-
-    document.body.appendChild(this.canvas);
-};
-
-Screen.prototype.drawSprite = function(sp, x, y) {
-    this.ctx.drawImage(sp.img, sp.x, sp.y, sp.w, sp.h, x, y, sp.w, sp.h);
-};
-
-// Sprite
-function Sprite(img, x, y, w, h) {
-    this.img = img;
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-};
-
-// InputHandeler
-function InputHandeler() {
-    this.down = {};
-    this. pressed = {};
-
-    var _this = this;
-    document.addEventListener("keydown", function(evt) {
-        _this.down[evt.keyCode] = true;
-    });
-    document.addEventListener("keyup", function(evt) {
-        delete _this.down[evt.keyCode];
-        delete _this.pressed[evt.keyCode];
-    })
-};
-
-InputHandeler.prototype.isDown = function(code) {
-    return this.down[code];
-};
-
-InputHandeler.prototype.isPressed = function(code) {
-    if (this.pressed[code]) {
-        return false;
-    } else if (this.down[code]) {
-        return this.pressed[code] = true;
-    }
-    return false;
->>>>>>> 948611ddb798c2cc357436f9bd2dab317bff894c
 };
